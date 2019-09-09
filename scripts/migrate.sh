2e88f52e3c507c73f1038cfa2355dee08778b442
Koichiro Kondo <>
2019-09-09 10:38:01 +0900

    指定したディレクトリ内のSQLをまとめて実行できるように修正

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,6 +4,9 @@ host=localhost
 port=3306
 execute=false
 
+#----------------------------------------------
+#   オプションのチェック
+#----------------------------------------------
 while getopts :h:p:d:f:-: opt; do
     case $opt in
         h) host=$OPTARG
@@ -34,6 +37,10 @@ if [ "${filepath}" = "" ]; then
     exit 1
 fi
 
+
+#----------------------------------------------
+#   実行関数を定義
+#----------------------------------------------
 dry_run () {
     mysqldef -u${DB_USER} -p${DB_PASSWORD} -h${host} -P${port} ${database} --dry-run < ${filepath}
 }
@@ -42,13 +49,23 @@ execute () {
     mysqldef -u${DB_USER} -p${DB_PASSWORD} -h${host} -P${port} ${database} < ${filepath}
 }
 
-dry_run
 
-if "${execute}"; then
-    echo "start migration"
-    execute
-    echo "end migration"
-fi
+#----------------------------------------------
+#   ファイル名とディレクトリ名をスキャンして実行
+#----------------------------------------------
+migration_dir=db/migrations
+list=`find ${migration_dir} -type f -name \*.sql`
+for filepath in ${list[@]} ; do
+    dir=${filepath%/*}
+    database=${dir#${migration_dir}/}
+
+    dry_run
+    if "${execute}"; then
+        echo "start migration"
+        execute
+        echo "end migration"
+    fi
+done
 
 exit;
 
